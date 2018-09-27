42e2cb003947aaaad5b3db300214c4468225ebea
Michael Trans <mich282q@edu.easj.dk>
2018-09-27 08:48:17 +0200

    test Hallo World

---
+++ Test/Hallo_World_Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Hallo_World_Test {
+    @Test
+
+    public void test() {
+        Testing testing = new Testing();
+        assertEquals("Hallo World");
+
+    }
+}
